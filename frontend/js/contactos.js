64982c11cbc9833d84e3b1b63ea29545f252c244
Daiana45 <dbogado.ipf@gmail.com>
2026-09-01 23:00:15 -0300

    Agregue validacion JS al formulario de contacto

---
+++ frontend/js/contactos.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('formContacto');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+const terminos = document.getElementById('terminos');
+
+const errorNombre = document.getElementById('error-nombre');
+const errorEmail = document.getElementById('error-email');
+const errorAsunto = document.getElementById('error-asunto');
+const errorMensaje = document.getElementById('error-mensaje');
+
+const mensajeExito = document.getElementById('mensajeExito');
+
+
+
+
+
+form.addEventListener('submit', function (evento) {
+    evento.preventDefault();
+    let esValido = true;
+
+    if(nombre.value.trim() === '') {
+        errorNombre.textContent = 'El nombre es obligatorio';
+        esValido = false
+    } else {
+        errorNombre.textContent = '';
+    }
+
+    const valorEmail = email.value.trim();
+    const tieneArroba = valorEmail.includes('@');
+    const tienePunto = valorEmail.includes('.');
+    const posicionArroba = valorEmail.indexOf('@');
+    const posicionPunto = valorEmail.indexOf('.');
+
+    if (valorEmail === '') {
+        errorEmail.textContent = 'El email es obligatorio';
+        esValido = false;
+    } else if (!tieneArroba || !tienePunto || posicionPunto < posicionArroba) {
+        errorEmail.textContent = 'Ingresa un email valido';
+        esValido = false;
+    } else {
+        errorEmail.textContent = '';
+    }
+
+    if (asunto.value === '') {
+        errorAsunto.textContent = 'Selecciona un asunto';
+        esValido = false;
+    } else {
+        errorAsunto.textContent = '';
+    }
+
+    if (mensaje.value.trim() === '') {
+        errorMensaje.textContent = 'El mensaje es obligatorio';
+        esValido = false;
+    } else {
+        errorMensaje.textContent = '';
+    }
+
+    if (!terminos.checked) {
+        esValido = false;
+    } 
+    if (esValido) {
+        mensajeExito.classList.remove('d-none');
+        form.reset();
+
+        setTimeout(function (){
+            mensajeExito.classList.add('d-none');
+    },4000);
+    } else {
+        mensajeExito.classList.add('d-none')
+    }
+});
