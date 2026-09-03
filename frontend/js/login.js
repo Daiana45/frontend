95b1a7726a57b91f2fb85d4c30b97258d39585e9
Daiana45 <dbogado.ipf@gmail.com>
2026-09-03 20:49:20 -0300

    feat(login): agregar funcionalidad de mostrar/ocultar contraseña con icono dell ojo

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,1 +1,15 @@
-document.getElementById('DOMco')
+const contraseña = document.getElementById("password-login");
+const oculto = document.getElementById("toggle-login");
+
+oculto.addEventListener("click", () => {
+    const inputType = contraseña.getAttribute("type");
+    if (inputType === "password") {
+        contraseña.setAttribute("type", "text")
+    } else  {
+        contraseña.setAttribute("type", "password")
+    }
+})
+
+const usuario = document.getElementById("usuario");
+const boton = document.getElementById("btn-auth");
+
