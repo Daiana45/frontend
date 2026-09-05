389a61ef505fe318efe2b2e0bbd6796f12c1a62a
Daiana45 <dbogado.ipf@gmail.com>
2026-09-04 23:46:57 -0300

    feat: agregue funcionalidad del toggle de contraseña para el registro

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -14,7 +14,8 @@ const usuario = document.getElementById("Usuario");
 const boton = document.getElementById("btn-auth");
 const mensajeError = document.getElementById("mensaje-error");
 
-boton.addEventListener("click", () => {
+boton.addEventListener("click", (event) => {
+    event.preventDefault();
     const valorUsuario = usuario.value;
     const valorContraseña = contraseña.value;
 
@@ -27,4 +28,20 @@ boton.addEventListener("click", () => {
 
     console.log("Usuario:", valorUsuario);
     console.log("Contraseña:", valorContraseña);
+});
+
+
+
+
+
+const passwordRegistrate = document.getElementById("passwordRegistrate");
+const ojoRegistro = document.getElementById("toggle-registrate");
+
+ojoRegistro.addEventListener("click", () =>{
+    const type =passwordRegistrate.getAttribute("type");
+    if (type === "password") {
+        passwordRegistrate.setAttribute("type", "text")
+    } else  {
+        passwordRegistrate.setAttribute("type", "password")
+    }
 });
